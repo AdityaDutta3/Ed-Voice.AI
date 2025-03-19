c0a7b276cac90d38aa3cfc652ff71517def7cf84
Aditya Dutta <duttaadi@sheridancollege.ca>
2025-03-19 18:09:46 -0400

    Authentication

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@stackframe/stack";
 
 export default function Home() {
   return (
     <div>
       <h1>Aditya Dutta</h1>
       <Button>Submit</Button>
+      <UserButton/>
     </div>
   );
 }
