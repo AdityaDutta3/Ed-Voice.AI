1ccbfff0a8853981063b7aff16835f734834398e
Aditya Dutta <duttaadi@sheridancollege.ca>
2025-03-19 21:09:21 -0400

    UseContext

--- app/AuthProvider.jsx
+++ app/AuthProvider.jsx
@@ -1,11 +1,13 @@
 import { api } from "@/convex/_generated/api";
 import { useUser } from "@stackframe/stack";
 import { useMutation } from "convex/react";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { UserContext } from "./_context/UserContext";
 
 function AuthProvider({ children }) {
   const user = useUser();
   const CreateUser = useMutation(api.users.createUser);
+  const [userData,setUserData] = useState();
 
   useEffect(() => {
     console.log(user);
@@ -18,9 +20,14 @@ function AuthProvider({ children }) {
       email: user.primaryEmail,
     });
     console.log(result);
+    setUserData(result)
   };
 
-  return <div>{children}</div>;
+  return (
+    <div>
+      <UserContext.Provider value={{userData,setUserData}} >{children}</UserContext.Provider>
+    </div>
+  );
 }
 
 export default AuthProvider;
